655f80fb0d961632a8ac8a53d1dcba260bc11fc7
Dante P <dantejpuglisi@bixlabs.com>
2017-12-29 10:32:15 -0300

    Initial commit

---
+++ Schools Manager/Schools Manager/APIClient.swift
@@ -0,0 +1,35 @@
+//
+//  APIClient.swift
+//  Schools Manager
+//
+//  Created by Dante Puglisi on 12/29/17.
+//  Copyright © 2017 Dante Puglisi. All rights reserved.
+//
+
+import Alamofire
+
+class APIClient {
+  static func register(withUsername username: String, password: String) {
+    print("Sending request")
+    let parameters: Parameters = ["username": username, "password": password]
+    Alamofire.request("http://127.0.0.1:5000/register", method: .post, parameters: parameters).responseJSON { response in
+      if let json = response.result.value {
+        print("JSON: \(json)")
+      }
+    }
+  }
+  
+  static func login(withUsername username: String, password: String) {
+    print("Sending request")
+    let parameters: Parameters = ["username": username, "password": password]
+    Alamofire.request("http://127.0.0.1:5000/auth", method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON { response in
+      if let json = response.result.value as? [String: AnyObject] {
+        print("JSON: \(json)")
+        if let access_token = json["access_token"] {
+          print("Access token is: \(access_token)")
+          UserDefaults.standard.set(access_token, forKey: "access_token")
+        }
+      }
+    }
+  }
+}
---
+++ Schools Manager/Schools Manager/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Schools Manager
+//
+//  Created by Dante Puglisi on 12/29/17.
+//  Copyright © 2017 Dante Puglisi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var usernameTextField: UITextField!
+  @IBOutlet weak var passwordTextField: UITextField!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  @IBAction func registerButtonPressed(_ sender: Any) {
+    guard let username = usernameTextField.text, let password = passwordTextField.text, username.count > 0, password.count > 0 else {
+      return
+    }
+    APIClient.register(withUsername: usernameTextField.text!, password: passwordTextField.text!)
+  }
+  
+  @IBAction func loginButtonPressed(_ sender: Any) {
+    guard let username = usernameTextField.text, let password = passwordTextField.text, username.count > 0, password.count > 0 else {
+      return
+    }
+    APIClient.login(withUsername: usernameTextField.text!, password: passwordTextField.text!)
+  }
+  
+}
+
